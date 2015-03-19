08d26ff4c0f16665e36c451fcca7c75c3d697c96
TYM-laptop <tym1060326@163.com>
2015-03-19 19:40:50 +0800

    使用审核程序更新的channel类

--- channel.py
+++ channel.py
@@ -7,49 +7,63 @@ import requests
 import config
 
 class Channel:
+    url_get = config.URL['danmaku']
+    url_post = config.URL['exam_post']
+
     _channel = ''
-    url_root = 'http://dm.tuna.moe/'
-    url_exam_get = urljoin(url_root, '/api/v1/channels/{channel}/danmaku/exam')
-    url_exam_post = urljoin(url_root, '/api/v1/channels/{channel}/danmaku')
-    url_danmaku = urljoin(url_root, '/api/v1/channels/{channel}/danmaku')
     _get_headers = {}
     _post_headers = {'content-type': 'application/json; charset=utf8'}
 
-    def __init__(self, channel, uuid, sub_passwd, pub_passwd=None):
+    def __init__(self, channel, uuid, sub_passwd=None, pub_passwd=None):
         self._channel = channel
-        self.url_exam_get = self.url_exam_get.format(channel=self._channel)
-        self.url_exam_post = self.url_exam_post.format(channel=self._channel)
-        self.url_danmaku = self.url_danmaku.format(channel=self._channel)
+        self.url_get = self.url_get.format(channel=self._channel)
+        self.url_post = self.url_post.format(channel=self._channel)
+
         self._get_headers['X-GDANMAKU-SUBSCRIBER-ID'] = uuid
-        self._get_headers['X-GDANMAKU-AUTH-KEY'] = sub_passwd
+        self._get_headers['X-GDANMAKU-AUTH-KEY'] = \
+                    sub_passwd if sub_passwd else ''
         # self._post_headers['X-GDANMAKU-SUBSCRIBER-ID'] = uuid
-        if pub_passwd:
-            self._post_headers['X-GDANMAKU-AUTH-KEY'] = pub_passwd
-        else:
-            self._post_headers['X-GDANMAKU-AUTH-KEY'] = ''
+        self._post_headers['X-GDANMAKU-AUTH-KEY'] = \
+                    pub_passwd if pub_passwd else ''
         self._post_headers['X-GDANMAKU-TOKEN'] = 'APP:'
 
 
     def get_danmaku(self):
-        # res = requests.get(self.url_exam_get, headers=self._get_headers)
-        res = requests.get(self.url_danmaku, headers=self._get_headers)
-        return json.loads(res.text)
+        res = requests.get(self.url_get, headers=self._get_headers)
+        try:
+            print(res.text)
+            return json.loads(res.text)
+        except ValueError as e:
+            return {}
 
 
     def post_danmaku(self, danmaku):
         if type(danmaku) == type(''):
-            res = requests.post(self.url_exam_post, data=danmaku,
+            res = requests.post(self.url_post, data=danmaku,
                     headers=self._post_headers)
         elif type(danmaku) == type({}) or type(danmaku) == type([]):
-            res = requests.post(self.url_exam_post, data=json.dumps(danmaku),
+            res = requests.post(self.url_post, data=json.dumps(danmaku),
                    headers=self._post_headers)
         return res.text
+
+
+class ExamChannel(Channel):
+    url_get = config.URL['exam_get']
+    url_post = config.URL['exam_post']
+
+    def __init__(self, channel, uuid, exam_passwd, sub_passwd=None,
+                    pub_passwd=None):
+        Channel.__init__(self, channel, uuid, sub_passwd, pub_passwd)
+
+        self._get_headers['X-GDANMAKU-AUTH-KEY'] = exam_passwd
+        self._post_headers['X-GDANMAKU-EXAM-KEY'] = exam_passwd
+
 
 
 if __name__ == '__main__':
     import time
 
-    c = Channel('demo', 'uuid', '', '')
+    c = ExamChannel('demo', 'uuid', '', '')
     s = c.post_danmaku({'color': 'green', 'position': 'fly', 'content': '超长弹幕超长弹幕超长弹幕超长弹幕超长弹幕超长弹幕超长弹幕'})
     print(s)
     while True:
